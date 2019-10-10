7ade7643742e3150e2992ff58e73b11360d7e38d
Jas Delgado <jas@jasdelgado.com>
2019-10-10 12:18:34 -0700

    investigate clicking on globe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Globe from 'worldwind-react-globe';
 import './App.css';
 
 const App = props => {
+	const globeRef = useRef(null);
+	const [initialized, setInitialized] = useState(false);
+
+	const onClickGlobe = item => {
+		console.log(item);
+	};
+
+	const onUpdate = data => {
+		console.log(data);
+	};
+
+	useEffect(() => {
+		console.log(globeRef.current);
+		globeRef.current.armClickDrop(onClickGlobe);
+	}, [globeRef]);
+
 	return (
 		<div className='page'>
-			<Globe arm />
+			<Globe ref={globeRef} onUpdate={onUpdate} />
 			<div className='fullscreen-item'>
 				<div className='bigstats'>
 					<h1>20,123 visitors</h1>
