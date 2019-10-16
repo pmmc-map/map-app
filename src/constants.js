f2c57c1130271dba6452d62c5fda243bbcb55cf1
Jas Delgado <jas@jasdelgado.com>
2019-10-16 16:38:10 -0700

    add useful constants for app state. why doesnt javascript have enums!

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const DEFAULT_SCREEN = 0;
+export const PIN_DROP_INSTRUCTIONS = 1;
+export const PIN_DROP_BEGIN = 2;
+export const MOUSE_DOWN = 3;
+export const MOUSE_UP = 4;
+export const MOUSE_MOVE = 5;
+export const MOUSE_NONE = 6;
