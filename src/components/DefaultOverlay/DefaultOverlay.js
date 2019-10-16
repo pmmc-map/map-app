beb2f445f871bd0e6353dd4b1a8dc3c7fe31d904
Jas Delgado <jas@jasdelgado.com>
2019-10-16 16:38:39 -0700

    get stats from props so they can be updated

--- src/components/DefaultOverlay/DefaultOverlay.js
+++ src/components/DefaultOverlay/DefaultOverlay.js
@@ -3,16 +3,16 @@ import React from 'react';
 /*
  * this is the default overlay that shows when we're not dropping a pin
  * should be shown when nobody's interacting with the screen
+ * TODO: add commas to numbers when digits > 3
  */
-const DefaultOverlay = props => (
-	<>
+const DefaultOverlay = ({ numVisitors, numCountries }) => (
+	<div className='default-overlay'>
 		<div className='bigstats'>
-			<h1>20,123 visitors</h1>
-			<h3>3 countries</h3>
-			<h3>23 numbers</h3>
+			<h1>{numVisitors} visitors</h1>
+			<h3>{numCountries} countries</h3>
 		</div>
 		<h1 className='drop-pin-cta'>Touch anywhere to start pin drop</h1>
-	</>
+	</div>
 );
 
 export default DefaultOverlay;
