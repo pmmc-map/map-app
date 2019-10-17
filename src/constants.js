6a0ac25d549927209076a44e963009509f68173f
Jas Delgado <jas@jasdelgado.com>
2019-10-17 15:00:07 -0700

    add more pin drop modes

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 export const DEFAULT_SCREEN = 0;
 export const PIN_DROP_INSTRUCTIONS = 1;
 export const PIN_DROP_BEGIN = 2;
-export const MOUSE_DOWN = 3;
-export const MOUSE_UP = 4;
-export const MOUSE_MOVE = 5;
-export const MOUSE_NONE = 6;
+export const PIN_DROP_CONFIRM = 3;
+export const PIN_DROP_DONE = 4;
+export const MOUSE_DOWN = 5;
+export const MOUSE_UP = 6;
+export const MOUSE_MOVE = 7;
+export const MOUSE_NONE = 8;
