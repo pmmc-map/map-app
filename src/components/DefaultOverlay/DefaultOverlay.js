bd7b5854aaf291e59cee297754916c2acf6d64dd
Tons-of-Carls <carlsim66@yahoo.com>
2019-10-19 21:23:30 -0700

    added basic donate popup

--- src/components/DefaultOverlay/DefaultOverlay.js
+++ src/components/DefaultOverlay/DefaultOverlay.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import PropTypes from 'prop-types';
+import DonatePopup from "./DonatePopup"
 
 /*
  * this is the default overlay that shows when we're not dropping a pin
  * should be shown when nobody's interacting with the screen
  * TODO: add commas to numbers when digits > 3
  */
-const DefaultOverlay = ({ numVisitors, numCountries }) => (
-	<div className='default-overlay'>
-		<div className='bigstats'>
-			<h1 className='header-visitors'>{numVisitors} visitors</h1>
-			<h3 className='visitor-substats'>{numCountries} countries</h3>
-		</div>
-		<h1 className='drop-pin-cta'>Touch anywhere to start pin drop</h1>
+const DefaultOverlay = ({ numVisitors, numCountries }) => {
+	const [displayDonatePopup, setDisplayDonatePopup] = useState(false);
+	return (
+		<div className='default-overlay'>
 
-		<button className='button button-donate'>Donate</button>
-	</div>
-);
+			<div className='bigstats'>
+				<h1 className='header-visitors'>{numVisitors} visitors</h1>
+				<h3 className='visitor-substats'>{numCountries} countries</h3>
+			</div>
+
+			<h1 className='drop-pin-cta'>Touch anywhere to start pin drop</h1>
+
+			<button className='button button-donate' onClick={()=>{
+				setDisplayDonatePopup(true)
+			}}>
+				Donate
+			</button>
+			{
+				displayDonatePopup ?
+					(
+						<DonatePopup
+							onReturnClick={()=>{
+								setDisplayDonatePopup(false)
+							}}
+						/>
+					) : null
+			}
+
+		</div>
+	);
+};
+
 
 DefaultOverlay.propTypes = {
 	numVisitors: PropTypes.number.isRequired,
