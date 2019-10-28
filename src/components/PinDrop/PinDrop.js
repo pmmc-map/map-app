045cf7c331dc6541632469a4a6ee32b5610c97e0
Jas Delgado <jas@jasdelgado.com>
2019-10-28 13:31:27 -0700

    make sure help popup isnt showing when pin confirmation popup showing

--- src/components/PinDrop/PinDrop.js
+++ src/components/PinDrop/PinDrop.js
@@ -11,7 +11,13 @@ const PinDropOverlay = ({
 	const [isHelpShowing, setIsHelpShowing] = useState(false);
 	return (
 		<div className='pin-drop-overlay'>
-			{isHelpShowing ? (
+			<div className='default-overlay'>
+				<div className='bigstats'>
+					<h1 className='header-visitors'>Where are you from?</h1>
+				</div>
+			</div>
+
+			{isHelpShowing && !isConfirmPopupShowing ? (
 				<HelpModal onClick={() => setIsHelpShowing(false)} />
 			) : null}
 			{isConfirmPopupShowing ? (
@@ -24,12 +30,13 @@ const PinDropOverlay = ({
 				Cancel
 			</button>
 			<button
+				disabled={isConfirmPopupShowing}
 				className='button button-help'
 				onClick={() =>
 					setIsHelpShowing(isHelpShowing => !isHelpShowing)
 				}
 			>
-				{isHelpShowing ? 'Dismiss' : 'Help'}
+				{isHelpShowing && !isConfirmPopupShowing ? 'Dismiss' : 'Help'}
 			</button>
 		</div>
 	);
