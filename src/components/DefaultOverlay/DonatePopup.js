8f4feeb8df984b63a95bc59bedc5b43528db18f7
Jas Delgado <jas@jasdelgado.com>
2019-10-29 10:22:06 -0700

    use relative sizes and flex, slightly improve styles

--- src/components/DefaultOverlay/DonatePopup.js
+++ src/components/DefaultOverlay/DonatePopup.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-const DonatePopup = ({onReturnClick})=>{
+const DonatePopup = ({ onReturnClick }) => {
+	useEffect(() => {
+		console.log('test');
+
+		return () => console.log('goodbye');
+	}, []);
 	return (
 		<div
-			className={"donate-background"}
+			className={'donate-background'}
 			onClick={e => {
+				onReturnClick();
 				e.stopPropagation();
 			}}
 		>
-			<div className={"donate-container"}>
-				<div className={"popup-title-container"}>
+			<div className='donate-container'>
+				<div className='popup-title-container'>
 					<h1>Donate to PMMC</h1>
 				</div>
-				<div className={"popup-body"}>
+				<div className='popup-body'>
 					<p>
-						Open the <strong>Camera app on your smartphone</strong> and <strong>scan the QR code below</strong> to donate to the Pacific Marine Mammal Center
+						Open the <strong>Camera app on your smartphone</strong>{' '}
+						and <strong>scan the QR code below</strong> to donate to
+						the Pacific Marine Mammal Center
 					</p>
 					<div>
-						<img src={"../../../public/donateQRcode.png"}/>
+						<img src={'../../../public/donateQRcode.png'} />
 					</div>
 
-					<button onClick={onReturnClick} className='button'>Return to Map</button>
+					<p>
+						Your donation will help to xxx, xxxxxx, and xxxxxxxxx
+						xxxx xxxxxxx.
+					</p>
+
+					<button onClick={onReturnClick} className='button'>
+						Return to Map
+					</button>
 				</div>
 			</div>
 		</div>
