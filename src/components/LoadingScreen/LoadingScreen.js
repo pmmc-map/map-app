97f02736e081ced1eac5bfbe967bcc1343d81041
Jas Delgado <jas@jasdelgado.com>
2019-10-30 14:06:15 -0700

    add loading screen

---
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const LoadingScreen = () => (
+	<div className='loader-page'>
+		<img
+			className='loader-circle'
+			src='../../../../public/circleloader.png'
+		/>
+	</div>
+);
+
+export default LoadingScreen;
