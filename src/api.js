311a2085bf113ebe16cfea73362f1cb9173dde5e
Jas Delgado <jas@jasdelgado.com>
2019-11-19 12:04:25 -0800

    use external backend

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_API_URL = 'http://127.0.0.1:5000/';
+const BASE_API_URL = 'http://54.183.19.24/';
 
 export const getPinInfo = async position => {
 	const response = await fetch(BASE_API_URL + 'api/geocoder', {
